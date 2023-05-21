6e6e2d1ba2b8f3ed09319f527f8a6dba9bba99aa
Ol900ivas <112936701+Ol900ivas@users.noreply.github.com>
2023-05-21 12:26:32 +0000

    Deploying to gh-pages from @ Ol900ivas/test1@834e08773103297017c611cc56074132933e0cf2 🚀

--- static/js/415.8e0a780c.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,r){r.r(t);var c=r(184);t.default=function(){return(0,c.jsx)("h1",{children:"Wellcome to our site!"})}}}]);
-//# sourceMappingURL=415.8e0a780c.chunk.js.map
